ea465830e915961bbecdcf3cd7e35438da91acf3
rcopra <137560494+rcopra@users.noreply.github.com>
2026-09-01 15:13:15 +0200

    fix

--- main.go
+++ main.go
@@ -89,10 +89,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 10,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Println("server listening")
 	log.Fatal(srv.ListenAndServe())
 }
